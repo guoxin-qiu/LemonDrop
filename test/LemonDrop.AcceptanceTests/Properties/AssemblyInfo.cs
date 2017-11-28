6c126524be9fa9e90427b7390fb6b28b5ef1f6ed
Denis <guoxin.qiu@outlook.com>
2017-11-28 18:45:05 +0800

    delete chinese characters in assembly infos

--- test/LemonDrop.AcceptanceTests/Properties/AssemblyInfo.cs
+++ test/LemonDrop.AcceptanceTests/Properties/AssemblyInfo.cs
@@ -1,13 +1,12 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("LemonDrop.AcceptanceTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("SYDQ")]
 [assembly: AssemblyProduct("LemonDrop.AcceptanceTests")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyCopyright("Copyright © SYDQ 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -15,6 +14,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("85989bfd-b952-447d-bc85-18b8ce9dd047")]
 
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- test/LemonDrop.UnitTests/Properties/AssemblyInfo.cs
+++ test/LemonDrop.UnitTests/Properties/AssemblyInfo.cs
@@ -1,13 +1,12 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("LemonDrop.UnitTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("SYDQ")]
 [assembly: AssemblyProduct("LemonDrop.UnitTests")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyCopyright("Copyright © SYDQ 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -15,6 +14,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("ff75f5a7-2ceb-414a-a132-cd667098d14c")]
 
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
