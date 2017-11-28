ff6d38a6deed0a46dea6881e6e469327b929ac38
Denis <guoxin.qiu@outlook.com>
2017-11-28 17:51:27 +0800

    add specflow inject manage sample

---
+++ test/LemonDrop.AcceptanceTests/Support/InjectManage.cs
@@ -0,0 +1,23 @@
+﻿using BoDi;
+using System;
+using TechTalk.SpecFlow;
+
+namespace LemonDrop.AcceptanceTests.Support
+{
+    [Binding, Scope(Tag = "BookStore")]
+    public class InjectManage
+    {
+        private readonly IObjectContainer _objectContainer;
+
+        public InjectManage(IObjectContainer objectContainer)
+        {
+            _objectContainer = objectContainer;
+        }
+
+        [BeforeScenario]
+        public void InitializeClock()
+        {
+            //_objectContainer.RegisterTypeAs<SystemClock, IClock>();
+        }
+    }
+}
