3302fbc387ef06095e67bc78089f006de568dfbb
Denis <guoxin.qiu@outlook.com>
2017-11-29 09:40:03 +0800

    use xunit for unit testing

---
+++ test/LemonDrop.UnitTests/SampleTest.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LemonDrop.UnitTests
+{
+    public class SampleTest
+    {
+        [Fact(Skip = "Test")]
+        public void FirstTest()
+        {
+            var a = 1;
+            var b = 1;
+            a.Should().Equals(b);
+        }
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(2, 2)]
+        [InlineData(3, 4)]
+        public void SecondTest(int firstNumber, int secondNumber)
+        {
+            firstNumber.Should().Equals(secondNumber);
+        }
+    }
+}
